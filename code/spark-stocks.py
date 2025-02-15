8f92b6d6921e3fae7cf3fb36a9e800c435df76dc
cpriest7 <ctpriest12@earthlink.net>
2025-02-15 12:20:40 -0500

    Clean up spark-stock.py

--- code/spark-stocks.py
+++ code/spark-stocks.py
@@ -1,11 +1,11 @@
-# --- MEGA CELL ---
-
 # import necessary packages
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType, IntegerType
 from pyspark.sql.window import Window
 from pyspark.sql.functions import lit
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # create spark session
 spark = SparkSession.builder.appName("StockData").getOrCreate()
@@ -25,22 +25,11 @@ stock_schema = StructType([
 local_path = "./data/AAPL.csv" 
 # Create apple stock data Spark DataFrame
 apple_df = spark.read.format("csv").option("header", "true").schema(stock_schema).load(local_path)
-#apple_df.show(10) # show top 10 points
 
-# use the following command to install pandas - ON PYSPARK NOTEBOOK
-#sc.install_pypi_package("pandas")
-# Now you can import the library as normal
-import pandas as pd
 # Create a pandas dataframe that holds the information from the apple dataframe
 apple_pandas_df = apple_df.toPandas()
 apple_pandas_df['Date'] = pd.to_datetime(apple_pandas_df['Date'])
-#apple_pandas_df.head(10) # show top 10 points in apple pandas df
 
-# use the following command to install matplotlib - ON PYSPARK NOTEBOOK
-#sc.install_pypi_package("matplotlib")
-import matplotlib.pyplot as plt
-
-# CREATE GRAPH
 # Create the visualization using matplotlib
 plt.figure(figsize=(12, 6))
 plt.plot(apple_pandas_df['Date'], apple_pandas_df['Close'], label='Close', c='red')
